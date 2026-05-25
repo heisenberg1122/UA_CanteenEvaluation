5134b52710ad54736bb4583508193a33ff627e14
heisenberg1122 <cjmquiambao.student@ua.edu.ph>
2026-05-25 18:36:50 +0800

    Add Backend Render url in FrontEnd

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -7,7 +7,7 @@ import {
 } from 'recharts';
 import { Camera, LayoutDashboard, FileText, LogOut, ShieldCheck, X, Star, Key, Hash, ShieldAlert, Search, Download, AlertTriangle, Clock, Terminal, Trash2, ChevronLeft, ChevronRight, Loader2, Store, Trophy, Medal, Award, Users } from 'lucide-react';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://ua-canteen-backend-0tew.onrender.com';
 
 export default function AdminDashboard({ navigate }) {
   const [feedbacks, setFeedbacks] = useState([]);
